0530ebbd957d6baf97cdb4f4508e8666a4b30943
borji <borjitaea@gmail.com>
2022-05-25 21:03:33 +0200

    Initial commit

---
+++ src/components/Row.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="row">
+    <div class="leftcolumn">
+      <div class="card" v-for="data in data" :key="data.id">
+        <h2>{{data.title}}</h2>
+        <h5>{{data.date}}</h5>
+        <div class="resumen" style="height:200px;">
+          <p>{{data.author}}</p>
+          <p>{{data.body}}</p>
+        </div>
+      </div>
+    </div>
+    <div class="rightcolumn">
+      <div class="card">
+        <h3>Destacados</h3>
+        <div class="card" v-for="data in data" :key="data.id">
+          <div class="resumen"><p>{{ data.body }}</p></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import data from '../components/data.json'
+export default {
+  name: "Row.vue",
+  props: data,
+  data(){
+    return{data}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/TopNav.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="topnav">
+    <a href="#">Manual</a>
+    <a href="#">Manual</a>
+    <a href="#">Manual</a>
+    <a href="#" style="float:right">Login para editar/añadir?</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TopNav.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
